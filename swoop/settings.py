2f215f9f8ada942b39552702fef1e87dd1314788
Nic1Mic <Nic1Mic@users.noreply.github.com>
2026-06-29 01:18:25 +0100

    Fix Cloudinary static compatibility

--- swoop/settings.py
+++ swoop/settings.py
@@ -136,10 +136,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
@@ -182,3 +180,5 @@ STORAGES = {
         "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
+
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
